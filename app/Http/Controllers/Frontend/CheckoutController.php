e691cf110de6787f49d3d1557f4cd7c8912edbd1
teyebhouda <teyebhouda1994@gmail.com>
2024-01-02 15:27:58 +0100

    stock fix

--- app/Http/Controllers/Frontend/CheckoutController.php
+++ app/Http/Controllers/Frontend/CheckoutController.php
@@ -23,7 +23,7 @@ use Illuminate\Http\Request;
 use Notification;
 use Config;
 use Session;
-
+use Auth;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Str;
 
@@ -93,9 +93,41 @@ class CheckoutController extends Controller
                 
                 $product = $cart->product_variation->product;
 
-                /*if($product->max_purchase_qty >= $cart->qty && $cart->qty >= $product->min_purchase_qty){
-                    $productVariationStock = $cart->product_variation->product_variation_stock ? $cart->product_variation->product_variation_stock->stock_qty : 0;
-                   /* if ($cart->qty > $productVariationStock) {
+
+                $apiUrl = env('API_CATEGORIES_URL');
+        
+                if (Auth::check() && Auth::user()->user_type == 'customer')
+            {
+            $response = Http::get($apiUrl . 'ListeDePrixWeb/' . Auth::user()->CODETIERS);
+            }else{
+            
+                $response = Http::get($apiUrl . 'ListeDePrixWeb/');
+        
+            }
+                $produitsApi = $response->json();
+        
+                
+                
+                
+                foreach ($produitsApi as $produitApi) {
+                    
+                    
+                    $apiStock = $produitApi['StockActual'];
+                   
+        
+                    if($produitApi['codeabarre'] == $product->slug ){
+        
+                     
+                        $product->stock_qty = $apiStock;
+                       
+        
+                    }
+                }
+
+
+                if($product->max_purchase_qty >= $cart->qty && $cart->qty >= $product->min_purchase_qty){
+                    $productVariationStock = $product->stock_qty ;
+                   if ($cart->qty > $productVariationStock) {
                         $message = $cart->product_variation->product->collectLocalization('name') . ' ' . localize('Est en rupture de stoc');
                         flash($message)->error();
                         return back();
@@ -105,7 +137,7 @@ class CheckoutController extends Controller
 
                     flash($message)->error();
                     return back();
-                } */
+                } 
             }
             
 
